408fa009ae9259bd562ac870cc25a3d28ed933cc
Erik Reiter <erik.reiter@stud.hs-mannheim.de>
2021-10-24 21:07:14 +0200

    fixed bootstrap import

---
+++ impressum.php
@@ -0,0 +1,5 @@
+<?php include("header.php")?>
+<body>
+    <h1>ich bin das impressum</h1>
+</body>
+<?php include("footer.php")?>
--- index.php
+++ index.php
@@ -1,10 +1,10 @@
-<?php include("header.html");?>
+<?php include("header.php");?>
 
 <!-- Begin page content -->
 <main role="main" class="container">
       <h1 class="mt-5">Sticky footer with fixed navbar</h1>
       <p class="lead">Pin a fixed-height footer to the bottom of the viewport in desktop browsers with this custom HTML and CSS. A fixed navbar has been added with <code>padding-top: 60px;</code> on the <code>body &gt; .container</code>.</p>
-      <p>Back to <a href="../sticky-footer/">the default sticky footer</a> minus the navbar.</p>
+
     </main>
 
-<?php include("footer.html");?>
+<?php include("footer.php");?>
